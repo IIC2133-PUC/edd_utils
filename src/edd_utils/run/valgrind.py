51d1b751f26431612be6dbf4ff852e3506ae0c74
Benjamín Vicente <benjavicente@uc.cl>
2024-07-05 21:45:40 -0400

    fix valgrind syntax bug

--- src/edd_utils/run/valgrind.py
+++ src/edd_utils/run/valgrind.py
@@ -12,7 +12,7 @@ def check_leaks_and_memory_errors(args: CMD, *, cwd: StrOrBytesPath | None = Non
     valgrind_args = ["valgrind", *args]
     valgrind_output = run(valgrind_args, cwd=cwd, text=True, stdout=DEVNULL, stderr=PIPE)
 
-    leaks_ok = "All heap blocks were freed -- no leaks are possible" in valgrind_output.stderr:
+    leaks_ok = "All heap blocks were freed -- no leaks are possible" in valgrind_output.stderr
     mem_ok = "ERROR SUMMARY: 0 errors from 0 contexts" in valgrind_output.stderr
 
     return leaks_ok, mem_ok
