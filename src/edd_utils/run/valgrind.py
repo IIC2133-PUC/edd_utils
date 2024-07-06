5fbb7b4da4380e89d7b9f588d2b1ea81dfb6b1bd
Benjamín Vicente <benjavicente@uc.cl>
2024-07-05 21:24:08 -0400

    fix valgrind undefined vars

--- src/edd_utils/run/valgrind.py
+++ src/edd_utils/run/valgrind.py
@@ -12,9 +12,7 @@ def check_leaks_and_memory_errors(args: CMD, *, cwd: StrOrBytesPath | None = Non
     valgrind_args = ["valgrind", *args]
     valgrind_output = run(valgrind_args, cwd=cwd, text=True, stdout=DEVNULL, stderr=PIPE)
 
-    if "All heap blocks were freed -- no leaks are possible" in valgrind_output.stderr:
-        leaks_ok = True
-    if "ERROR SUMMARY: 0 errors from 0 contexts" in valgrind_output.stderr:
-        mem_ok = True
+    leaks_ok = "All heap blocks were freed -- no leaks are possible" in valgrind_output.stderr:
+    mem_ok = "ERROR SUMMARY: 0 errors from 0 contexts" in valgrind_output.stderr
 
     return leaks_ok, mem_ok
